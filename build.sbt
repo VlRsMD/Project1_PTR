5cd5c64d66c8a2dde04003f2e4c4fd5e6a744417
VlRsMD <90247966+VlRsMD@users.noreply.github.com>
2023-03-14 22:58:20 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,19 @@
+name := "Project1_PTR"
+
+version := "0.1"
+
+scalaVersion := "2.13.10"
+
+libraryDependencies += "com.typesafe.akka" % "akka-actor_2.13" % "2.7.0"
+
+libraryDependencies += "com.lihaoyi" %% "requests" % "0.8.0"
+
+libraryDependencies += "org.jsoup" % "jsoup" % "1.15.4"
+
+libraryDependencies += "com.typesafe.akka" %% "akka-http" % "10.5.0"
+
+// libraryDependencies += "org.json4s" %% "json4s-core" % "4.0.6"
+
+// libraryDependencies += "org.json4s" %% "json4s-jackson" % "4.0.6"
+
+
---
+++ src/main/scala/week1/readTweets.scala
@@ -0,0 +1,30 @@
+package week1
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import akka.actor._
+import scala.collection.mutable.ListBuffer
+
+class readTweets extends Actor {
+  def receive = {
+    case "Start" => {
+      var tweets: Document = Jsoup.connect("http://localhost:4000//tweets/1").get();
+      val tweetsListB: ListBuffer[String] = new ListBuffer[String]
+      val split1 = tweets.text().split(":")
+      for (i<-0 to split1.length-1) {
+        // println(spl1(i))
+        if (split1(i).contains("text") && split1(i+1).contains("source") && i < split1.length-1){
+          val s2 = split1(i+1).split("\"")
+          tweetsListB += s2(1)
+        }
+      }
+      val tweetsList = tweetsListB.toList
+    }
+  }
+}
+
+object read extends App {
+  val system = ActorSystem()
+  val readerActor = system.actorOf(Props(new readTweets))
+  readerActor ! "Start"
+}
